9ecad8c3b2cb8f16409325818501a7d4f5106e98
Javier <javelie@outlook.com>
2020-01-27 20:58:32 -0700

    Initial Commit

---
+++ templateSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  templateSwiftUI
+//
+//  Created by José Javier Cueto Mejía on 27/01/20.
+//  Copyright © 2020 José Javier Cueto Mejía. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
